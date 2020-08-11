d01bcb84aa4ff2fa9a9ce0ba82b6b7b507bcd8c8
lucasmsa <lucasmsea@outlook.com>
2020-08-10 22:47:09 -0300

    first commit, menu functionality done

---
+++ Assets/Scripts/AboutScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class AboutScript : MonoBehaviour
+{
+    public void BackToMenu() 
+    {
+        SceneManager.LoadScene("AppScene");
+    }
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartButton() 
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void AboutButton()
+    {
+        Debug.Log("Meu Pal No Seu Cu");
+        SceneManager.LoadScene("AboutScene");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
